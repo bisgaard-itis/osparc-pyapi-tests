5d21ed4f8d0911b2f50ce98836f6915934b4ffed
Werner Van Geit <werner.vangeit@epfl.ch>
2024-04-08 14:20:49 +0200

    Saving

--- noninter1/run.sh
+++ noninter1/run.sh
@@ -4,5 +4,6 @@ set -ex
 
 source ./testenv/bin/activate
 
+pip install /home/vangeit/src/osparc-simcore-clients/clients/python/artifacts/dist/*.whl
 rm -rf output_1
 python run_study.py
--- noninter1/run_study.py
+++ noninter1/run_study.py
@@ -1,19 +1,17 @@
 """Test osparc client API"""
 
-import time
+import json
 import shutil
+import time
 import zipfile
+from pathlib import Path
 
 import osparc
 import osparc.api
 import osparc_client
-from pathlib import Path
 
-osparc_cfg = osparc.Configuration(
-    host="10.43.103.149.nip.io:8006",
-    username="test_T1QyAxKBUX",
-    password="0dsHA6zdYDNEtwNKsXZHBQq8eHuPbd",
-)
+osparc_conf = json.loads(Path("osparc_conf.json").read_text())
+osparc_cfg = osparc.Configuration(**osparc_conf)
 
 with osparc.ApiClient(osparc_cfg) as api_client:
     studies_api = osparc_client.StudiesApi(api_client)
@@ -27,8 +25,10 @@ with osparc.ApiClient(osparc_cfg) as api_client:
     # template_id = (
     #    "f580a480-bb9a-11ee-b73f-02420a000006"  # PythonRunnerStudyService
     # )
-    template_id = "02621c5a-bbba-11ee-ba85-02420a000022"
+    # template_id = "02621c5a-bbba-11ee-ba85-02420a000022"
+    # template_id = "a13d566e-c05b-11ee-95bf-02420a000008"
     # job_id = "32ccb81c-bc34-11ee-ba85-02420a000022"
+    template_id = "5b01fb90-f59f-11ee-9635-02420a140047"
 
     print(studies_api.list_study_ports(study_id=template_id))
 
@@ -39,28 +39,32 @@ with osparc.ApiClient(osparc_cfg) as api_client:
     test_data_file = osparc.api.FilesApi(api_client).upload_file(
         file=Path("input.data")
     )
+    test_json_file = osparc.api.FilesApi(api_client).upload_file(
+        file=Path("input.json")
+    )
 
     new_job = studies_api.create_study_job(
         study_id=template_id,
         job_inputs={
             "values": {
-                "PythonCode1": test_py_file,
-                "DataFile1": test_data_file,
+                # "InputNumber1": 0.5,
+                # "InputInteger1": 6,
+                "InputFile1": test_json_file,
             }
         },
     )
 
-    print(studies_api.start_study_job(study_id=template_id, job_id=new_job.id))
+    studies_api.start_study_job(study_id=template_id, job_id=new_job.id)
 
     job_status = studies_api.inspect_study_job(
         study_id=template_id, job_id=new_job.id
     )
 
-    while job_status.state != "SUCCESS" and job_status != "FAILED":
+    while job_status.state != "SUCCESS" and job_status.state != "FAILED":
         job_status = studies_api.inspect_study_job(
             study_id=template_id, job_id=new_job.id
         )
-        print(job_status.state)
+        print(f"Status: [{job_status.state}]")
 
         time.sleep(1)
 
@@ -84,8 +88,10 @@ with osparc.ApiClient(osparc_cfg) as api_client:
 
     output_file = Path(output_filename)
 
+    # print(output_file.resolve())
+
     with zipfile.ZipFile(output_file, "r") as zip_ref:
         zip_ref.extractall()
 
-    outdata_file = Path("output_1/output.data")
+    outdata_file = Path("output.json")
     print(outdata_file.read_text())
